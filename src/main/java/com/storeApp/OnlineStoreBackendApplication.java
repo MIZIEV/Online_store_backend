05554e56db13e8d079a0ff8e5fdb3fa90d68b809
Miziev <mizzzev@outlook.com>
2024-01-30 18:21:33 +0200

    - created empty project

---
+++ src/main/java/com/storeApp/OnlineStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.storeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/storeApp/OnlineStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
