fac6a0f0480f7f3d288239be3e187989fa3228bd
Miziev <mizzzev@outlook.com>
2024-02-14 17:09:49 +0200

    - created all services interface for each entities - created one implementation for Product entity with two methods (addNewProduct() & getAllProducts())

---
+++ src/main/java/com/storeApp/service/UserService.java
@@ -0,0 +1,4 @@
+package com.storeApp.service;
+
+public interface UserService {
+}
---
+++ src/main/java/com/storeApp/service/implementation/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package com.storeApp.service.implementation;
+
+import com.storeApp.models.Product;
+import com.storeApp.repository.ProductRepository;
+import com.storeApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void addNewProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+
+    }
+}
