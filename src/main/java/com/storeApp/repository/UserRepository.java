2d340906c8e6e2cde4c313bcd285ca5d0512c923
Miziev <mizzzev@outlook.com>
2024-02-14 16:52:11 +0200

    - created all repositories for each entities

---
+++ src/main/java/com/storeApp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.storeApp.repository;
+
+import com.storeApp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
