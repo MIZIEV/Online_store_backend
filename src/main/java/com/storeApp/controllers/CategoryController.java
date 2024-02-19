4132a49da445b52a5f50c5615747cec9e32cbfee
Miziev <mizzzev@outlook.com>
2024-02-19 14:33:52 +0200

    - created new controller for Category entity with one method (addNewCategory())

---
+++ src/main/java/com/storeApp/controllers/CategoryController.java
@@ -0,0 +1,29 @@
+package com.storeApp.controllers;
+
+import com.storeApp.models.Category;
+import com.storeApp.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/category")
+@CrossOrigin("*")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addNewCategory(@RequestBody Category category){
+
+        categoryService.addNewCategory(category);
+
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
+}
