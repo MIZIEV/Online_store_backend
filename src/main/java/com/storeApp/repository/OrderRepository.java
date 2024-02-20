10701baedc7d6d392425aa5127cd547a4956826e
IvanKibets <ivan.kibets.pl@gmail.com>
2024-02-20 11:12:00 +0100

    add crud-functional for order entity

--- src/main/java/com/storeApp/repository/OrderRepository.java
+++ src/main/java/com/storeApp/repository/OrderRepository.java
@@ -4,6 +4,10 @@ import com.storeApp.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Optional<Order> findOrderById(long id);
 }
