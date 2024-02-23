efe129c768286f3008f74badccc5141440cece87
Miziev <mizzzev@outlook.com>
2024-02-23 15:18:43 +0200

    - continue implement security functional
    - created loginDto class
    - added login method in AuthService & implemented it in class
    - added login method in controller

--- src/main/java/com/storeApp/controllers/AuthController.java
+++ src/main/java/com/storeApp/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.storeApp.controllers;
 
+import com.storeApp.dto.LoginDto;
 import com.storeApp.dto.RegisterDto;
 import com.storeApp.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,12 @@ public class AuthController {
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
         String response = authService.register(registerDto);
 
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        String response = authService.login(loginDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/storeApp/service/implementation/AuthServiceImpl.java
+++ src/main/java/com/storeApp/service/implementation/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.storeApp.service.implementation;
 
+import com.storeApp.dto.LoginDto;
 import com.storeApp.dto.RegisterDto;
 import com.storeApp.models.Role;
 import com.storeApp.models.User;
@@ -7,6 +8,10 @@ import com.storeApp.repository.RoleRepository;
 import com.storeApp.repository.UserRepository;
 import com.storeApp.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +24,15 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public AuthServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
     @Override
@@ -56,5 +62,16 @@ public class AuthServiceImpl implements AuthService {
 
         userRepository.save(user);
         return "User registered successfully!!!";
+    }
+
+    @Override
+    public String login(LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsernameOrEmail(),
+                        loginDto.getPassword()
+                ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "User logged -in successfully!!!";
     }
 }
