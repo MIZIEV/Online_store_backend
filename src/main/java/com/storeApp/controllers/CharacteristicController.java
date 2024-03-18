3d9dbccb5b92c3566e21150bc740797d773c2469
Miziev <mizzzev@outlook.com>
2024-03-18 16:15:21 +0200

    - created dto class for characteristic & implemented it in controller with convert method

--- src/main/java/com/storeApp/controllers/CharacteristicController.java
+++ src/main/java/com/storeApp/controllers/CharacteristicController.java
@@ -1,8 +1,10 @@
 package com.storeApp.controllers;
 
+import com.storeApp.dto.ProductCharacteristicDto;
 import com.storeApp.models.ProductCharacteristic;
 import com.storeApp.service.ProductCharacteristicService;
 import com.storeApp.service.ProductService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +22,8 @@ public class CharacteristicController {
     private final ProductService productService;
 
     @Autowired
-    public CharacteristicController(ProductCharacteristicService characteristicService, ProductService productService) {
+    public CharacteristicController(ProductCharacteristicService characteristicService,
+                                    ProductService productService) {
         this.characteristicService = characteristicService;
         this.productService = productService;
     }
@@ -30,31 +33,38 @@ public class CharacteristicController {
 
         List<ProductCharacteristic> characteristicList = productService.getProductById(id).getCharacteristicList();
 
-        return new ResponseEntity<>(characteristicList,HttpStatus.OK);
+        return new ResponseEntity<>(characteristicList, HttpStatus.OK);
     }
 
     @PostMapping("/add")
     public ResponseEntity<?> addNewCharacteristic(@PathVariable("product_id") Long id,
-                                                  @RequestBody ProductCharacteristic characteristic) {
+                                                  @RequestBody ProductCharacteristicDto characteristicDto) {
 
-        characteristicService.addNewCharacteristic(characteristic,id);
+        characteristicService.addNewCharacteristic(convertToProductCharacteristic(characteristicDto), id);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{characteristic_id}")
-    public ResponseEntity<?> updateCharacteristic(@RequestBody ProductCharacteristic editedCharacteristic,
+    public ResponseEntity<?> updateCharacteristic(@RequestBody ProductCharacteristicDto editedCharacteristicDto,
                                                   @PathVariable("characteristic_id") Long id) {
-        ProductCharacteristic updatedCharacteristic = characteristicService.updateCharacteristic(editedCharacteristic, id);
+        ProductCharacteristic updatedCharacteristic =
+                characteristicService.updateCharacteristic(convertToProductCharacteristic(editedCharacteristicDto), id);
 
         return new ResponseEntity<>(updatedCharacteristic, HttpStatus.OK);
     }
 
     @DeleteMapping("/{characteristic_id}")
-    public ResponseEntity<?> deleteCharacteristic(@PathVariable("characteristic_id") Long id){
+    public ResponseEntity<?> deleteCharacteristic(@PathVariable("characteristic_id") Long id) {
 
         characteristicService.deleteCharacteristic(id);
 
-        return new ResponseEntity<>("Characteristic deleted successfully.",HttpStatus.OK);
+        return new ResponseEntity<>("Characteristic deleted successfully.", HttpStatus.OK);
+    }
+
+    private ProductCharacteristic convertToProductCharacteristic(ProductCharacteristicDto characteristicDto) {
+        ModelMapper modelMapper = new ModelMapper();
+
+        return modelMapper.map(characteristicDto, ProductCharacteristic.class);
     }
 }
