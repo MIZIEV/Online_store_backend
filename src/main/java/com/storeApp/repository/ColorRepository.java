3c4e38f3f91c257872aa62b5b14698eb6b8b4c2f
Miziev <mizzzev@outlook.com>
2024-04-03 18:53:49 +0300

    - added custom method in ColorRepository for getting Set<Color> from DB directly

--- src/main/java/com/storeApp/repository/ColorRepository.java
+++ src/main/java/com/storeApp/repository/ColorRepository.java
@@ -2,11 +2,18 @@ package com.storeApp.repository;
 
 import com.storeApp.models.Color;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
-public interface ColorRepository extends JpaRepository<Color,Long> {
+public interface ColorRepository extends JpaRepository<Color, Long> {
     Optional<Color> findColorById(Long id);
+
+    @Query("SELECT c FROM Color c WHERE c.id IN:colorIds")
+    Set<Color> findByColorsIds(@Param("colorIds") List<Long> colorIds);
 }
