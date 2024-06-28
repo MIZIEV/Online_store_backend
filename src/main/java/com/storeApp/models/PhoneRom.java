fbe90f90bdb04857cf91a4ed66f8e48e7af863f3
Miziev <mizzzev@outlook.com>
2024-06-28 22:07:38 +0300

    - improve phone functional - added price field in PhoneRom entity & dto (now every phone with particular rom size has price)

--- src/main/java/com/storeApp/models/PhoneRom.java
+++ src/main/java/com/storeApp/models/PhoneRom.java
@@ -16,6 +16,9 @@ public class PhoneRom {
     @Column(name = "rom_size")
     private Short romSize;
 
+    @Column(name = "price")
+    private Integer price;
+
     @ManyToOne
     @JsonBackReference
     @JoinColumn(name = "phone_id",referencedColumnName = "id")
@@ -23,9 +26,10 @@ public class PhoneRom {
 
     public PhoneRom(){}
 
-    public PhoneRom(Long id, Short romSize, Phone phone) {
+    public PhoneRom(Long id, Short romSize, Integer price, Phone phone) {
         this.id = id;
         this.romSize = romSize;
+        this.price = price;
         this.phone = phone;
     }
 
@@ -51,5 +55,13 @@ public class PhoneRom {
 
     public void setPhone(Phone phone) {
         this.phone = phone;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
     }
 }
--- src/main/java/com/storeApp/repository/PhoneRomRepository.java
+++ src/main/java/com/storeApp/repository/PhoneRomRepository.java
@@ -2,8 +2,10 @@ package com.storeApp.repository;
 
 import com.storeApp.models.PhoneRom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.Set;
@@ -12,6 +14,10 @@ import java.util.Set;
 public interface PhoneRomRepository extends JpaRepository<PhoneRom, Long> {
 
     Optional<PhoneRom> findPhoneRomById(long id);
+
+    @Modifying
+    @Transactional
+    void deleteAllByPhoneId(Long id);
 
     @Query("SELECT DISTINCT p.romSize FROM PhoneRom p")
     Set<Short> findDistinctPhoneRom();
