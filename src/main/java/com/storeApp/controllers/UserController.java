0925762a760b420f0198dd16861f001ceb3ece01
Miziev <mizzzev@outlook.com>
2024-07-06 20:50:33 +0300

    - added functional for deleting user ( in service & controller)

--- src/main/java/com/storeApp/controllers/UserController.java
+++ src/main/java/com/storeApp/controllers/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -40,6 +39,12 @@ public class UserController {
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public ResponseEntity<?> changeUserPassword(@PathVariable("email") String email, @RequestBody LoginDto loginDto) {
         return new ResponseEntity<>(userService.changeUserPassword(email, loginDto.getPassword()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/remove")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    public ResponseEntity<?> deleteUser(@PathVariable("email") String email) {
+        return new ResponseEntity<>(userService.deleteUser(email), HttpStatus.OK);
     }
 
     private User convertToUser(UserDto userDto) {
