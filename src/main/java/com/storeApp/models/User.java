4ff2b88b99f883198b85cec42a90278f3a4a8d50
Miziev <mizzzev@outlook.com>
2024-07-10 23:04:56 +0300

    - improved user functional (delete user) now after deleting orders don't delete

--- src/main/java/com/storeApp/models/User.java
+++ src/main/java/com/storeApp/models/User.java
@@ -40,7 +40,7 @@ public class User {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "orderOwner", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "orderOwner", cascade = CascadeType.PERSIST)
     @JsonManagedReference
     private List<Order> orderList;
 
--- src/main/java/com/storeApp/repository/OrderRepository.java
+++ src/main/java/com/storeApp/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.storeApp.repository;
 
+import com.storeApp.models.User;
 import com.storeApp.models.order.Order;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -19,5 +21,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Transactional
     @Query(value = "DELETE FROM order_selected_phone WHERE order_id = :orderId", nativeQuery = true)
     void deleteOrderPhoneByOrderId(@Param("orderId") Long orderId);
+
+    List<Order> findByOrderOwner(User user);
 
 }
